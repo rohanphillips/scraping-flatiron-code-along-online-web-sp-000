311b37477ea72aec2241d1713f9fba6a36e495c9
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-11 16:28:46 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -8,6 +8,9 @@ class Scraper
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
   end
+  def get_courses
+    get_page.css(".post")
+  end
 
   def print_courses
     self.make_courses
